ded7fa94cfd246170ed71eab94981f44c6847c42
senntou <wataru_00117@icloud.com>
2024-01-05 01:56:56 +0900

    feat:各ボタンに体の各部位が含まれるかの判定機能を実装

--- src/features/Cameras/scripts/lib/canvas/virtualButtons.ts
+++ src/features/Cameras/scripts/lib/canvas/virtualButtons.ts
@@ -17,17 +17,17 @@ export const virtualButtons = {
   grid_width: 3,
   height_rate: [0.25, 0.5, 0.25], // 各行の幅の比
   width_rate: [0.25, 0.5, 0.25], // 各列の幅の比
-  buttons_visibility: [true, false, true, true, false, true, true, false, true],
+  buttons_visibility: [
+    [true, false, true],
+    [true, false, true],
+    [true, false, true],
+  ],
   valid_keypoints: ["right_wrist", "left_wrist", "right_ankle", "left_ankle"],
-  buttons_state: {
-    none: 0,
-    left_hand: 1,
-    right_hand: 2,
-    left_ankle: 3,
-    right_ankle: 4,
-  },
   // 0: 通常状態のカラー      1:押されている状態のカラー
   colors: ["rgba(100, 100, 100, 0.8)", "rgba(255, 100, 0, 0.3)"],
+
+  height_rate_sum: Array(),
+  width_rate_sum: Array(),
 };
 
 export function buttons_draw_grid() {
@@ -35,21 +35,21 @@ export function buttons_draw_grid() {
   const canvas = getCanvasElement();
   const context = getCanvasContext(canvas);
 
-  let width_rate_sum = [0];
-  let height_rate_sum = [0];
+  virtualButtons.height_rate_sum = [0];
+  virtualButtons.width_rate_sum = [0];
   let cnt = 0;
   for (let i = 0; i < virtualButtons.grid_width; i++) {
     cnt += virtualButtons.width_rate[i];
-    width_rate_sum.push(cnt);
+    virtualButtons.width_rate_sum.push(cnt);
   }
   cnt = 0;
   for (let i = 0; i < virtualButtons.grid_height; i++) {
     cnt += virtualButtons.height_rate[i];
-    height_rate_sum.push(cnt);
+    virtualButtons.height_rate_sum.push(cnt);
   }
 
   for (let i = 0; i < virtualButtons.grid_width; i++) {
-    let x = canvas.width * width_rate_sum[i];
+    let x = canvas.width * virtualButtons.width_rate_sum[i];
 
     context.beginPath();
     context.moveTo(x, 0); //始点
@@ -58,7 +58,7 @@ export function buttons_draw_grid() {
     context.stroke();
   }
   for (let i = 0; i < virtualButtons.grid_height; i++) {
-    let y = canvas.height * height_rate_sum[i];
+    let y = canvas.height * virtualButtons.height_rate_sum[i];
 
     context.beginPath();
     context.moveTo(0, y); //始点
@@ -75,33 +75,57 @@ export function buttons_draw(poses: Pose[]) {
 
   let keypoints: Keypoint[] = poses.length != 0 ? poses[0].keypoints : Array();
 
-  let height_rate_sum = [0];
-  let width_rate_sum = [0];
   let cnt = 0;
   for (let i = 0; i < virtualButtons.grid_height; i++) {
     cnt += virtualButtons.height_rate[i];
-    height_rate_sum.push(cnt);
+    virtualButtons.height_rate_sum.push(cnt);
   }
   cnt = 0;
   for (let i = 0; i < virtualButtons.grid_width; i++) {
     cnt += virtualButtons.width_rate[i];
-    width_rate_sum.push(cnt);
+    virtualButtons.width_rate_sum.push(cnt);
   }
 
   // 仮想ボタンの描画
   for (let i = 0; i < virtualButtons.grid_height; i++) {
     for (let j = 0; j < virtualButtons.grid_width; j++) {
-      if (!virtualButtons.buttons_visibility[i * virtualButtons.grid_width + j])
-        continue;
+      if (!virtualButtons.buttons_visibility[i][j]) continue;
 
       let x1, y1, x2, y2: number;
-      y1 = canvas.height * height_rate_sum[i];
-      x1 = canvas.width * width_rate_sum[j];
-      y2 = canvas.height * height_rate_sum[i + 1];
-      x2 = canvas.width * width_rate_sum[j + 1];
+      y1 = canvas.height * virtualButtons.height_rate_sum[i];
+      x1 = canvas.width * virtualButtons.width_rate_sum[j];
+      y2 = canvas.height * virtualButtons.height_rate_sum[i + 1];
+      x2 = canvas.width * virtualButtons.width_rate_sum[j + 1];
 
       context.fillStyle = virtualButtons.colors[0];
+
+      for (let target of virtualButtons.valid_keypoints) {
+        if (contains(i, j, keypoints, target)) {
+          context.fillStyle = virtualButtons.colors[1];
+        }
+      }
+
       context.fillRect(x1, y1, x2 - x1, y2 - y1);
     }
   }
+}
+
+function findKeypointByName(
+  keypoints: Keypoint[],
+  targetName: String,
+): Keypoint | undefined {
+  return keypoints.find((keypoint) => keypoint.name === targetName);
+}
+
+function contains(h: number, w: number, keypoints: Keypoint[], target: String) {
+  let x1, y1, x2, y2: number;
+  let canvas = getCanvasElement();
+  y1 = canvas.height * virtualButtons.height_rate_sum[h];
+  x1 = canvas.width * virtualButtons.width_rate_sum[w];
+  y2 = canvas.height * virtualButtons.height_rate_sum[h + 1];
+  x2 = canvas.width * virtualButtons.width_rate_sum[w + 1];
+
+  let pnt: Keypoint = findKeypointByName(keypoints, target) as Keypoint;
+
+  return x1 <= pnt.x && pnt.x <= x2 && y1 <= pnt.y && pnt.y <= y2;
 }
