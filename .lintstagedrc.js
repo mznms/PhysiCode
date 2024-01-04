3d308bd6ead7d327272f7add739e9c3afef845ec
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-01-04 18:07:18 +0900

    feat: prettier, eslint, git hook, lint-staged の設定

---
+++ .lintstagedrc.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+module.exports = {
+  "src/**/*.{js,jsx,ts,tsx}": [
+    "prettier --write",
+    buildEslintCommand,
+    () => "tsc",
+  ],
+  "!src/**/*.{js,jsx,ts,tsx}": "prettier --ignore-unknown --write",
+};
