74caf407f86a4a957bc66cb32407a80f368314ef
senntou <wataru_00117@icloud.com>
2024-01-09 17:41:12 +0900

    feat:削除を除く、全ての文字の入力機能を作成した
    8種類の文字に対して入力判定を行う機能を実装した。

--- src/features/Cameras/scripts/lib/canvas/checkInput.ts
+++ src/features/Cameras/scripts/lib/canvas/checkInput.ts
@@ -4,14 +4,7 @@ import { Button, Base, Trigger } from "../types/types";
 import { virtualButtons, contains } from "./virtualButtons";
 
 /**
- * 入力判定をするための関数を作る
- *
- * base : Array<String>
- * trigger  Array<String>
- */
-
-/**
- * まず機能として、　ベース　トリガー　の２つを受け取る必要があるね
+ * まず機能として、　ベース　トリガー　の２つを受け取る必要がある
  * ベース：　足の位置　どの足がどの場所にあるかを指定する必要がある
  * トリガー：　手の位置　どの手がどの場所に入るかを指定する
  *      type1 片手を特定のエリアに入れる
@@ -33,11 +26,11 @@ function makeCheckInput(base: Base, trigger: Trigger, character: String) {
   let currentState = false;
 
   function checkInput(keypoints: Keypoint[]) {
+    // ベースを満たしているか判定
+    if (!contains(base.left_ankle, keypoints, "left_ankle")) return;
+    if (!contains(base.right_ankle, keypoints, "right_ankle")) return;
     let previousState = currentState;
     currentState = false;
-    // ベースを満たしているか判定
-    // if (!contains(base.left_ankle, keypoints, "left_ankle")) return;
-    // if (!contains(base.right_ankle, keypoints, "right_ankle")) return;
 
     // トリガーを満たしているか判定
     let satisfyTrigger = false;
@@ -74,14 +67,74 @@ export function makeFuncGetInputs() {
     3: { h: 1, w: 2 },
     5: { h: 1, w: 0 },
     6: { h: 2, w: 2 },
-    7: { h: 2, 2: 1 },
+    7: { h: 2, w: 1 },
     8: { h: 2, w: 0 },
+    null: { h: -1, w: -1 },
   };
   let funcs = [
     makeCheckInput(
       { left_ankle: buttons[6], right_ankle: buttons[8] },
       { left_wrist: buttons[5], right_wrist: buttons[5], needBothHands: false },
       "+",
+    ),
+    makeCheckInput(
+      { left_ankle: buttons[6], right_ankle: buttons[8] },
+      { left_wrist: buttons[3], right_wrist: buttons[3], needBothHands: false },
+      "-",
+    ),
+    makeCheckInput(
+      { left_ankle: buttons[6], right_ankle: buttons[7] },
+      {
+        left_wrist: buttons[0],
+        right_wrist: buttons["null"],
+        needBothHands: false,
+      },
+      ">",
+    ),
+    makeCheckInput(
+      { left_ankle: buttons[7], right_ankle: buttons[8] },
+      {
+        left_wrist: buttons["null"],
+        right_wrist: buttons[2],
+        needBothHands: false,
+      },
+      "<",
+    ),
+    makeCheckInput(
+      { left_ankle: buttons[8], right_ankle: buttons[7] },
+      {
+        left_wrist: buttons[0],
+        right_wrist: buttons["null"],
+        needBothHands: false,
+      },
+      ".",
+    ),
+    makeCheckInput(
+      { left_ankle: buttons[7], right_ankle: buttons[6] },
+      {
+        left_wrist: buttons["null"],
+        right_wrist: buttons[2],
+        needBothHands: false,
+      },
+      ",",
+    ),
+    makeCheckInput(
+      { left_ankle: buttons[6], right_ankle: buttons[7] },
+      {
+        left_wrist: buttons["null"],
+        right_wrist: buttons[2],
+        needBothHands: false,
+      },
+      "[",
+    ),
+    makeCheckInput(
+      { left_ankle: buttons[7], right_ankle: buttons[8] },
+      {
+        left_wrist: buttons[0],
+        right_wrist: buttons["null"],
+        needBothHands: false,
+      },
+      "]",
     ),
   ];
   function getInputs(keypoints: Keypoint[]) {
