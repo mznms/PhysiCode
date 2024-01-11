82c1a13837f7ecd656d91f97b7622842aaca8395
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-01-12 05:09:39 +0900

    feat: コンテンツを真ん中に表示するように変更

--- src/app/free-play/page.tsx
+++ src/app/free-play/page.tsx
@@ -1,10 +1,10 @@
-import { Code } from "@nextui-org/code";
 import { Cameras } from "@/features/Cameras/Cameras";
+import { Code } from "@/features/Code/Code";
 import { Input } from "@/features/Input/Input";
 
 export default function FreePlay() {
   return (
-    <main>
+    <main className="max-w-screen-xl mx-auto">
       <div className="mx-2 flex flex-col gap-6">
         <Code />
         <Input />
