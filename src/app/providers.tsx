493402fb89822a17726d3b1a714ca5b3dd97023b
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-01-13 05:22:13 +0900

    feat: Output コンポーネントを実装

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,13 +4,16 @@ import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { CodeProvider } from "@/features/Code/codeContext";
 import { MemoryProvider } from "@/features/Memory/memoryProvider";
+import { OutputProvider } from "@/features/Output/outputContext";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   return (
     <NextUIProvider navigate={router.push}>
       <CodeProvider>
-        <MemoryProvider>{children}</MemoryProvider>
+        <MemoryProvider>
+          <OutputProvider>{children}</OutputProvider>
+        </MemoryProvider>
       </CodeProvider>
     </NextUIProvider>
   );
