f42224bbeec4c9b3d52d07bfb094b26cab356d52
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-01-13 02:50:10 +0900

    refactor: navbar の分割したコンポーネントを一つのファイルにまとめた
    Navbar のコンポーネントでしか使うことはありえないのに別ファイルに切り出す必要もないと思ったゆえ

--- src/features/ui/NavigationBar/NavigationBar.tsx
+++ src/features/ui/NavigationBar/NavigationBar.tsx
@@ -1,9 +1,13 @@
 import { Navbar, NavbarContent, NavbarMenuToggle } from "@nextui-org/navbar";
-import { NavbarItem } from "@nextui-org/react";
+import {
+  Link,
+  NavbarBrand,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@nextui-org/react";
 import { PlusIcon } from "../Icon/PlusIcon";
-import { Brand } from "./Brand/Brand";
-import { Menu } from "./Menu/Menu";
-import { NavLinks } from "./NavLinks/NavLinks";
+import { RunningManIcon } from "../Icon/RunningManIcon";
 
 export function NavigatioBar() {
   return (
@@ -29,5 +33,63 @@ export function NavigatioBar() {
 
       <Menu />
     </Navbar>
+  );
+}
+
+function Brand() {
+  return (
+    <NavbarBrand>
+      <RunningManIcon size={40} />
+      <p className="font-bold">PhysiCode</p>
+    </NavbarBrand>
+  );
+}
+
+function Menu() {
+  return (
+    <NavbarMenu>
+      <NavbarMenuItem>
+        <Link className="w-full" color="foreground" href="/" size="lg">
+          ホーム
+        </Link>
+      </NavbarMenuItem>
+      <NavbarMenuItem>
+        <Link className="w-full" color="foreground" href="/free-play" size="lg">
+          フリーモード
+        </Link>
+      </NavbarMenuItem>
+      <NavbarMenuItem>
+        <Link
+          className="w-full"
+          color="foreground"
+          href="/challenge-play"
+          size="lg"
+        >
+          チャレンジモード
+        </Link>
+      </NavbarMenuItem>
+    </NavbarMenu>
+  );
+}
+
+function NavLinks() {
+  return (
+    <>
+      <NavbarItem>
+        <Link color="foreground" href="/">
+          ホーム
+        </Link>
+      </NavbarItem>
+      <NavbarItem>
+        <Link color="foreground" href="/free-play">
+          フリーモード
+        </Link>
+      </NavbarItem>
+      <NavbarItem>
+        <Link color="foreground" href="/challenge-play">
+          チャレンジモード
+        </Link>
+      </NavbarItem>
+    </>
   );
 }
