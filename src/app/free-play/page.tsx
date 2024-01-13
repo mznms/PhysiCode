d3b08e75be1737e89065c56d24551d2e04b9e587
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-01-14 01:00:15 +0900

    feat: BodyProgrammingEditor をページに表示

---
+++ src/app/free-play/page.tsx
@@ -0,0 +1,9 @@
+import { BodyProgrammingEditor } from "@/features/BodyProgrammingEditor/BodyProgrammingEditor";
+
+export default function FreePlayPage() {
+  return (
+    <main>
+      <BodyProgrammingEditor />
+    </main>
+  );
+}
