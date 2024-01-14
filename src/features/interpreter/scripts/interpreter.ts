51cb9b9c5c5e6b864b4f7780812bd2e87681f864
Masanari Mizuno <mznms@primu.la>
2024-01-15 04:01:21 +0900

    feat: BF の処理系をステップ実行できるよう改修

--- src/features/interpreter/scripts/interpreter.ts
+++ src/features/interpreter/scripts/interpreter.ts
@@ -1,5 +1,4 @@
 export class UnclosedBracketError extends Error {}
-export class TimeOutError extends Error {}
 export class MemoryBoundError extends Error {}
 
 /**
@@ -11,78 +10,98 @@ export class MemoryBoundError extends Error {}
  * - `]` から `[` へジャンプ時，対応するものが見つからなければ コード先頭へ代わりにジャンプ
  * - メモリは 9999 bytes (0-9998)
  * - メモリの領域外アクセスは `MemoryBoundError` をスロー
- * - 実行時間が 2 秒を超えた場合は `TimeOutError` をスロー
- * @param code BF のコード
- * @param input 入力文字列
- * @returns 出力文字列
  */
-export function interpret(code: string, input: string): string {
-  let mem: number[] = new Array(9999).fill(0);
-  let pointer = 0;
-  let output = "";
+export class BFInterpreter {
+  private readonly code: string;
+  private readonly input: string;
 
-  let codeIndex = 0;
-  let inputIndex = 0;
+  private mem: number[]; // メモリ
+  private pointer: number; // メモリのどこを現在指しているか
+  private output: string; // 出力文字列
+  private codeIndex: number; // コードの何文字目を現在指しているか
+  private inputIndex: number; // 入力文字列の何文字目を現在指しているか
 
-  const startTime = performance.now();
+  private stepCount: number; // ステップ実行が何回成功したか
 
-  while (codeIndex < code.length) {
-    const currentTime = performance.now();
-    const elapsedTime = currentTime - startTime;
+  /**
+   * BF のコードと入力文字列をセット
+   * @param code BF のコード
+   * @param input 入力
+   */
+  constructor(code: string, input: string) {
+    this.code = code;
+    this.input = input;
 
-    if (elapsedTime > 2000) {
-      throw new TimeOutError();
+    this.mem = new Array(9999).fill(0);
+    this.pointer = 0;
+    this.output = "";
+    this.codeIndex = 0;
+    this.inputIndex = 0;
+
+    this.stepCount = 0;
+  }
+
+  /**
+   * BF をステップ実行する．
+   * このメソッドを呼び出すたび，トークンを 1 つ読み出し，メモリと出力の状態を書き換える．
+   * @returns
+   */
+  stepwiseExecution(): void {
+    if (this.codeIndex >= this.code.length) {
+      return;
     }
 
-    const token = code.charAt(codeIndex);
+    const token = this.code.charAt(this.codeIndex);
 
     switch (token) {
       case ">":
-        pointer++;
-        if (pointer >= 9999) {
+        this.pointer++;
+        if (this.pointer >= 9999) {
           throw new MemoryBoundError();
         }
         break;
 
       case "<":
-        pointer--;
-        if (pointer < 0) {
+        this.pointer--;
+        if (this.pointer < 0) {
           throw new MemoryBoundError();
         }
         break;
 
       case "+":
-        mem[pointer] = fixMemValue(++mem[pointer]);
+        this.mem[this.pointer] = fixMemValue(++this.mem[this.pointer]);
         break;
 
       case "-":
-        mem[pointer] = fixMemValue(--mem[pointer]);
+        this.mem[this.pointer] = fixMemValue(--this.mem[this.pointer]);
         break;
 
       case ".":
-        output += String.fromCharCode(mem[pointer]);
+        this.output += String.fromCharCode(this.mem[this.pointer]);
         break;
 
       case ",":
-        if (inputIndex < input.length) {
-          mem[pointer] = fixMemValue(input.charCodeAt(inputIndex++));
+        if (this.inputIndex < this.input.length) {
+          this.mem[this.pointer] = fixMemValue(
+            this.input.charCodeAt(this.inputIndex++),
+          );
         } else {
-          mem[pointer] = 255;
+          this.mem[this.pointer] = 255;
         }
         break;
 
       case "[":
-        if (mem[pointer] === 0) {
+        if (this.mem[this.pointer] === 0) {
           let loopDepth = 1;
 
           while (loopDepth > 0) {
-            codeIndex++;
-            if (codeIndex >= code.length) {
+            this.codeIndex++;
+            if (this.codeIndex >= this.code.length) {
               throw new UnclosedBracketError();
             }
-            if (code.charAt(codeIndex) === "[") {
+            if (this.code.charAt(this.codeIndex) === "[") {
               loopDepth++;
-            } else if (code.charAt(codeIndex) === "]") {
+            } else if (this.code.charAt(this.codeIndex) === "]") {
               loopDepth--;
             }
           }
@@ -90,18 +109,18 @@ export function interpret(code: string, input: string): string {
         break;
 
       case "]":
-        if (mem[pointer] !== 0) {
+        if (this.mem[this.pointer] !== 0) {
           let loopDepth = 1;
 
           while (loopDepth > 0) {
-            codeIndex--;
-            if (codeIndex < 0) {
-              codeIndex = 0;
+            this.codeIndex--;
+            if (this.codeIndex < 0) {
+              this.codeIndex = 0;
               break;
             }
-            if (code.charAt(codeIndex) === "]") {
+            if (this.code.charAt(this.codeIndex) === "]") {
               loopDepth++;
-            } else if (code.charAt(codeIndex) === "[") {
+            } else if (this.code.charAt(this.codeIndex) === "[") {
               loopDepth--;
             }
           }
@@ -112,10 +131,52 @@ export function interpret(code: string, input: string): string {
         break;
     }
 
-    codeIndex++;
+    this.codeIndex++;
+
+    this.stepCount++;
+
+    return;
   }
 
-  return output;
+  /**
+   * 現在バッファされている出力文字列を出力する．
+   * @returns 現在の出力
+   */
+  getOutput(): string {
+    return this.output;
+  }
+
+  /**
+   * 現在のメモリをダンプする．
+   * @returns 現在のメモリ
+   */
+  getMemDump(): number[] {
+    return this.mem;
+  }
+
+  /**
+   * 現在のポインタ位置（メモリ配列の何番目を指しているか）を出力する．
+   * @returns 現在のポインタ位置
+   */
+  getPointer(): number {
+    return this.pointer;
+  }
+
+  /**
+   * 現在までに何回ステップ実行されたか（＝ `stepwiseExecution` により何回トークンを読んだか）を出力する．
+   * @returns 現在のステップ実行回数
+   */
+  getStepCount(): number {
+    return this.stepCount;
+  }
+
+  /**
+   * プログラムが終了状態かどうか通知する．
+   * @returns コードの実行が終了であるかの真偽値（`true` なら終了）．
+   */
+  isHalted(): boolean {
+    return this.codeIndex >= this.code.length;
+  }
 }
 
 /**
