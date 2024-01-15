16a51f88b2bd886a0511ad50d03a3d01f2f3b074
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-01-16 03:11:14 +0900

    feat: bf 画面のレイアウトの実装

--- src/features/BodyProgrammingEditor/BodyProgrammingEditor.tsx
+++ src/features/BodyProgrammingEditor/BodyProgrammingEditor.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { BF } from "../BF/BF";
 import { Cameras } from "../Cameras/Cameras";
 import { Result } from "./Result/Result";
 import { useBodyProgrammingEditor } from "./bodyProgrammingEditorContext";
@@ -8,10 +9,14 @@ export function BodyProgrammingEditor() {
 
   return (
     <>
-      {isOpen && (
+      {isOpen ? (
         <div className="lg:flex gap-2 items-stretch lg:mx-4">
           <Cameras />
           <Result />
+        </div>
+      ) : (
+        <div className="px-4 mx-auto">
+          <BF />
         </div>
       )}
     </>
