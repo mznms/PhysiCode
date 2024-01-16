4cda9f0326df2b8e3ac8699b688a24b7f0d7eb1e
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-01-17 02:41:00 +0900

    Revert "fix: vh ではなく dvh を使うようにした"
    This reverts commit 36977378f1726ce35a7ab5c292639d4e4dfac667.

--- src/features/Cameras/Cameras.tsx
+++ src/features/Cameras/Cameras.tsx
@@ -16,11 +16,11 @@ export function Cameras() {
       />
       <canvas
         id="camera-canvas"
-        className="scale-x-[-1] object-cover w-screen h-[100vw] lg:w-[min(calc(100dvh-64px-32px),calc(100vw-340px))] lg:h-[min(calc(100dvh-64px-32px),calc(100vw-340px))] shrink-0"
+        className="scale-x-[-1] object-cover w-screen h-[100vw] lg:w-[min(calc(100vh-64px-32px),calc(100vw-340px))] lg:h-[min(calc(100vh-64px-32px),calc(100vw-340px))] shrink-0"
       />
       <canvas
         id="canvas"
-        className="absolute top-0 left-0 scale-x-[-1] object-cover w-screen h-[100vw] lg:w-[min(calc(100dvh-64px-32px),calc(100vw-340px))] lg:h-[min(calc(100dvh-64px-32px),calc(100vw-340px))]"
+        className="absolute top-0 left-0 scale-x-[-1] object-cover w-screen h-[100vw] lg:w-[min(calc(100vh-64px-32px),calc(100vw-340px))] lg:h-[min(calc(100vh-64px-32px),calc(100vw-340px))]"
       />
       <div id="fps" className="absolute top-0 left-1">
         FPS : 0
