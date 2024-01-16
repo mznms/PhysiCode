36977378f1726ce35a7ab5c292639d4e4dfac667
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-01-16 16:30:10 +0900

    fix: vh ではなく dvh を使うようにした

--- src/features/BodyProgrammingEditor/Result/Result.tsx
+++ src/features/BodyProgrammingEditor/Result/Result.tsx
@@ -11,7 +11,7 @@ export function Result() {
       disableAnimation
       disableAutosize
       classNames={{
-        base: "absolute bottom-0 left-0 z-50 w-screen h-[calc(100vh-100vw-64px)] lg:relative lg:w-auto lg:min-w-64 lg:flex-grow lg:h-[min(calc(100vh-64px-32px),calc(100vw-340px))]",
+        base: "absolute bottom-0 left-0 z-50 w-screen h-[calc(100dvh-100vw-64px)] lg:relative lg:w-auto lg:min-w-64 lg:flex-grow lg:h-[min(calc(100dvh-64px-32px),calc(100vw-340px))]",
         inputWrapper: "flex-grow rounded-none lg:rounded-2xl",
         input: "text-4xl h-full",
       }}
