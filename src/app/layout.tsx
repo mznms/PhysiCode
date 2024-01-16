b50d14b6b6691000a262df0d7e6f5a0be51b0330
Key5n <k.nogami.472@stn.nitech.ac.jp>
2024-01-17 06:27:26 +0900

    feat: en -> ja

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body
         className={`${inter.className} dark text-foreground bg-background min-h-screen`}
       >
